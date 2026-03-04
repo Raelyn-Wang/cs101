035182297269c4e877aae454199aae1793b59d70
Raelyn-Wang <r80171@gmail.com>
2026-03-04 20:19:02 +0800

    Create 2.main.c

---
+++ 實戰演練/2.main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int a;
+    int b;
+    scanf("%d",&a);
+    scanf("%d",&b);
+    if(a>b){
+        printf("%d>%d",a,b);
+    }else if(a<b){
+        printf("%d<%d",a,b);
+    }else{
+        printf("%d=%d",a,b);
+    }
+    return 0;
+}
